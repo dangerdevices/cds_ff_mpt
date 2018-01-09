6f367213e6286eea4a4aff78ef3ad39d17071a20
Eric Chang <pkerichang@gmail.com>
2018-01-09 11:46:31 +0800

    1. updating TechInfoCDSFFMPT and technology configuration file.

--- templates_cds_ff_mpt/tech.py
+++ templates_cds_ff_mpt/tech.py
@@ -43,19 +43,91 @@ class TechInfoCDSFFMPT(TechInfoConfig):
         process_params['layout']['laygo_tech_class'] = None
         process_params['layout']['res_tech_class'] = None
 
-        TechInfo.__init__(self, 0.001, 1e-6, 'cds_ff_mpt', process_params)
+        TechInfoConfig.__init__(self, 0.001, 1e-6, 'cds_ff_mpt', process_params)
         self.idc_temp = process_params['layout']['em']['dc_temp']
         self.irms_dt = process_params['layout']['em']['rms_dt']
 
     def get_metal_em_specs(self, layer_name, w, l=-1, vertical=False, **kwargs):
-        return float('inf'), float('inf'), float('inf')
+        metal_type = self.get_layer_type(layer_name)
+        idc = self._get_metal_idc(metal_type, w, l, vertical, **kwargs)
+        irms = self._get_metal_irms(layer_name, w, **kwargs)
+        ipeak = float('inf')
+        return idc, irms, ipeak
+
+    def _get_metal_idc_factor(self, mtype, w, l):
+        return 1
+
+    def _get_metal_idc(self, metal_type, w, l, vertical, **kwargs):
+        if vertical:
+            raise NotImplementedError('Vertical DC current not supported yet')
+
+        inorm, woff = 1.0, 0.0
+        idc = inorm * self._get_metal_idc_factor(metal_type, w, l) * (w - woff)
+        idc_temp = kwargs.get('dc_temp', self.idc_temp)
+        return self.get_idc_scale_factor(idc_temp, metal_type) * idc * 1e-3
+
+    def _get_metal_irms(self, layer_name, w, **kwargs):
+        b = 0.0443
+        k, wo, a = 6.0, 0.0, 0.2
+
+        irms_dt = kwargs.get('rms_dt', self.irms_dt)
+        irms_ma = (k * irms_dt * (w - wo)**2 * (w - wo + a) / (w - wo + b))**0.5
+        return irms_ma * 1e-3
 
     def get_via_em_specs(self, via_name, bm_layer, tm_layer, via_type='square',
                          bm_dim=(-1, -1), tm_dim=(-1, -1), array=False, **kwargs):
-        return float('inf'), float('inf'), float('inf')
+        bm_type = self.get_layer_type(bm_layer)
+        tm_type = self.get_layer_type(tm_layer)
+        idc = self._get_via_idc(via_name, via_type, bm_type, tm_type, bm_dim,
+                                tm_dim, array, **kwargs)
+        # via do not have AC current specs
+        irms = float('inf')
+        ipeak = float('inf')
+        return idc, irms, ipeak
+
+    def _get_via_idc(self, vname, via_type, bm_type, tm_type,
+                     bm_dim, tm_dim, array, **kwargs):
+        if bm_dim[0] > 0:
+            bf = self._get_metal_idc_factor(bm_type, bm_dim[0], bm_dim[1])
+        else:
+            bf = 1.0
+
+        if tm_dim[0] > 0:
+            tf = self._get_metal_idc_factor(tm_type, tm_dim[0], tm_dim[1])
+        else:
+            tf = 1.0
+
+        factor = min(bf, tf)
+        if vname == '1x' or vname == '4':
+            if via_type == 'square':
+                idc = 0.1 * factor
+            elif via_type == 'hrect' or via_type == 'vrect':
+                idc = 0.2 * factor
+            else:
+                # we do not support 2X square via, as it has large
+                # spacing rule to square/rectangle vias.
+                raise ValueError('Unsupported via type %s' % via_type)
+        elif vname == '2x':
+            if via_type == 'square':
+                idc = 0.4 * factor
+            else:
+                raise ValueError('Unsupported via type %s' % via_type)
+        else:
+            raise ValueError('Unsupported via name %s and bm_type %s' % (vname, bm_type))
+
+        idc_temp = kwargs.get('dc_temp', self.idc_temp)
+        return self.get_idc_scale_factor(idc_temp, bm_type) * idc * 1e-3
 
     def get_res_em_specs(self, res_type, w, l=-1, **kwargs):
-        return float('inf'), float('inf'), float('inf')
+        idc_temp = kwargs.get('dc_temp', self.idc_temp)
+        idc_scale = self.get_idc_scale_factor(idc_temp, '', is_res=True)
+        idc = 1.0e-3 * w * idc_scale
+
+        irms_dt = kwargs.get('rms_dt', self.irms_dt)
+        irms = 1e-3 * (0.02 * irms_dt * w * (w + 0.5)) ** 0.5
+
+        ipeak = 5e-3 * 2 * w
+        return idc, irms, ipeak
 
     def add_cell_boundary(self, template, box):
         pass
@@ -132,10 +204,6 @@ class TechInfoCDSFFMPT(TechInfoConfig):
 
         return sp, sp2_list, sp3_list, dim, enc, arr_enc, arr_test2
 
-    @classmethod
-    def use_flip_parity(cls):
-        return False
-
     def get_min_space(self, layer_type, width, unit_mode=False, same_color=False):
         if layer_type == '1x':
             if same_color:
@@ -204,134 +272,3 @@ class TechInfoCDSFFMPT(TechInfoConfig):
             return math.ceil(0.0082 / width / res) * res
         else:
             raise ValueError('Unsupported layer type: %s' % layer_type)
-
-    def get_layer_id(self, layer_name):
-        if layer_name == 'LiPo' or layer_name == 'LiAct':
-            return 0
-        return int(layer_name[1:])
-
-    def get_layer_name(self, layer):
-        if layer == 0:
-            raise ValueError('Ambiguous layer name for layer ID = 0')
-        return 'M%d' % layer
-
-    def get_layer_type(self, layer_name):
-        if layer_name == 'LiPo' or layer_name == 'LiAct':
-            return layer_name
-        layer_id = self.get_layer_id(layer_name)
-        if 1 <= layer_id <= 3:
-            return '1x'
-        elif layer_id == 4:
-            return '4'
-        elif 5 <= layer_id <= 6:
-            return '2x'
-        else:
-            raise ValueError('Unsupported layer ID: %d' % layer_id)
-
-    def get_via_name(self, bot_layer_id):
-        top_layer_name = self.get_layer_name(bot_layer_id + 1)
-        return self.get_layer_type(top_layer_name)
-
-    def _get_metal_idc(self, metal_type, w, l, vertical, **kwargs):
-        if vertical:
-            raise NotImplementedError('Vertical DC current not supported yet')
-
-        inorm, woff = 1.0, 0.0
-        idc = inorm * self.get_metal_idc_factor(metal_type, w, l) * (w - woff)
-        idc_temp = kwargs.get('dc_temp', self.idc_temp)
-        return self.get_idc_scale_factor(idc_temp, metal_type) * idc * 1e-3
-
-    def _get_metal_irms(self, layer_name, w, **kwargs):
-        b = 0.0443
-        k, wo, a = 6.0, 0.0, 0.2
-
-        irms_dt = kwargs.get('rms_dt', self.irms_dt)
-        irms_ma = (k * irms_dt * (w - wo)**2 * (w - wo + a) / (w - wo + b))**0.5
-        return irms_ma * 1e-3
-
-    def get_metal_em_specs(self, layer_name, w, l=-1, vertical=False, **kwargs):
-        metal_type = self.get_layer_type(layer_name)
-        idc = self._get_metal_idc(metal_type, w, l, vertical, **kwargs)
-        irms = self._get_metal_irms(layer_name, w, **kwargs)
-        ipeak = float('inf')
-        return idc, irms, ipeak
-
-    def _get_via_idc(self, vname, via_type, bm_type, tm_type,
-                     bm_dim, tm_dim, array, **kwargs):
-        if bm_dim[0] > 0:
-            bf = self.get_metal_idc_factor(bm_type, bm_dim[0], bm_dim[1])
-        else:
-            bf = 1.0
-
-        if tm_dim[0] > 0:
-            tf = self.get_metal_idc_factor(tm_type, tm_dim[0], tm_dim[1])
-        else:
-            tf = 1.0
-
-        factor = min(bf, tf)
-        if vname == '1x' or vname == '4':
-            if via_type == 'square':
-                idc = 0.1 * factor
-            elif via_type == 'hrect' or via_type == 'vrect':
-                idc = 0.2 * factor
-            else:
-                # we do not support 2X square via, as it has large
-                # spacing rule to square/rectangle vias.
-                raise ValueError('Unsupported via type %s' % via_type)
-        elif vname == '2x':
-            if via_type == 'square':
-                idc = 0.4 * factor
-            else:
-                raise ValueError('Unsupported via type %s' % via_type)
-        else:
-            raise ValueError('Unsupported via name %s and bm_type %s' % (vname, bm_type))
-
-        idc_temp = kwargs.get('dc_temp', self.idc_temp)
-        return self.get_idc_scale_factor(idc_temp, bm_type) * idc * 1e-3
-
-    def get_via_em_specs(self, via_name, bm_layer, tm_layer, via_type='square',
-                         bm_dim=(-1, -1), tm_dim=(-1, -1), array=False, **kwargs):
-        bm_type = self.get_layer_type(bm_layer)
-        tm_type = self.get_layer_type(tm_layer)
-        idc = self._get_via_idc(via_name, via_type, bm_type, tm_type, bm_dim,
-                                tm_dim, array, **kwargs)
-        # via do not have AC current specs
-        irms = float('inf')
-        ipeak = float('inf')
-        return idc, irms, ipeak
-
-    def get_res_rsquare(self, res_type):
-        return 500
-
-    def get_res_width_bounds(self, res_type):
-        return 0.5, 5.0
-
-    def get_res_length_bounds(self, res_type):
-        return 0.5, 50.0
-
-    def get_res_min_nsquare(self, res_type):
-        return 1.0
-
-    def get_res_em_specs(self, res_type, w, l=-1, **kwargs):
-        idc_temp = kwargs.get('dc_temp', self.idc_temp)
-        idc_scale = self.get_idc_scale_factor(idc_temp, '', is_res=True)
-        idc = 1.0e-3 * w * idc_scale
-
-        irms_dt = kwargs.get('rms_dt', self.irms_dt)
-        irms = 1e-3 * (0.02 * irms_dt * w * (w + 0.5)) ** 0.5
-
-        ipeak = 5e-3 * 2 * w
-        return idc, irms, ipeak
-
-    def get_res_info(self, res_type, w, l, **kwargs):
-        # only one type of resistor
-        rsq = self.get_res_rsquare(res_type)
-        res = l / w * rsq
-        idc, irms, ipeak = self.get_res_em_specs(res_type, w, l=l, **kwargs)
-
-        return dict(
-            resistance=res,
-            idc=idc,
-            iac_rms=irms,
-            iac_peak=ipeak,
-        )
