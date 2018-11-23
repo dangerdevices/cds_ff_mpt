54bb9b87fd41a8a53c774dc727f07b72d2d690e8
Eric Chang <pkerichang@gmail.com>
2018-11-22 22:59:53 -0800

    added netlist configuration files.

---
+++ netlist_setup/bag_prim.v
@@ -0,0 +1,112 @@
+
+module nmos4_18(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module nmos4_svt(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module nmos4_lvt(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module nmos4_hvt(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module nmos4_standard(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module nmos4_fast(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module nmos4_low_power(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module pmos4_18(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module pmos4_svt(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module pmos4_lvt(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module pmos4_hvt(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module pmos4_standard(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module pmos4_fast(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
+
+module pmos4_low_power(
+    inout B,
+    inout D,
+    inout G,
+    inout S
+);
+endmodule
